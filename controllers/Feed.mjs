ce752bb0cd84b0bbdfdf71b3d8ef8538c69469ea
adamanandas <adamananda02@gmail.com>
2023-04-16 20:32:57 +0700

    Add validation for create post

--- controllers/Feed.mjs
+++ controllers/Feed.mjs
@@ -1,19 +1,44 @@
+import { validationResult } from "express-validator";
+
 const getPosts = (req, res, next) => {
-  res
-    .status(200)
-    .json({ posts: [{ title: "Posts", content: "Hello, World!" }] });
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Adam Ananda Santoso",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
 };
 
 const createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed",
+      errors: errors.array(),
+    });
+  }
 
   res.status(201).json({
     message: "Success to created post",
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title: title,
       content: content,
+      creator: {
+        name: "Adam Ananda Santoso",
+      },
+      createdAt: new Date(),
     },
   });
 };
--- routes/feed.mjs
+++ routes/feed.mjs
@@ -1,9 +1,17 @@
 import express from "express";
+import { body } from "express-validator";
 import * as FeedController from "../controllers/Feed.mjs";
 
 const router = express.Router();
 
 router.get("/posts", FeedController.getPosts);
-router.post("/post", FeedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 7 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  FeedController.createPost
+);
 
 export { router };
