8bb412e0ad74770053a5fa154dce860210971902
adamanandas <adamananda02@gmail.com>
2023-04-20 21:21:35 +0700

    Add middleware for all post routes

--- routes/feed.mjs
+++ routes/feed.mjs
@@ -1,27 +1,30 @@
 import express from "express";
 import { body } from "express-validator";
 import * as FeedController from "../controllers/Feed.mjs";
+import isAuth from "../middleware/isAuth.mjs";
 
 const router = express.Router();
 
-router.get("/posts", FeedController.getPosts);
+router.get("/posts", isAuth, FeedController.getPosts);
 router.post(
   "/post",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   FeedController.createPost
 );
-router.get("/post/:postId", FeedController.getPost);
+router.get("/post/:postId", isAuth, FeedController.getPost);
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   FeedController.updatePost
 );
-router.delete("/post/:postId", FeedController.deletePost);
+router.delete("/post/:postId", isAuth, FeedController.deletePost);
 
 export { router };
