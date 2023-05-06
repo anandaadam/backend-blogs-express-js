bae21ec86540f4bc03eb498c255b472d214bbd7a
adamanandas <adamananda02@gmail.com>
2023-05-06 17:58:54 +0700

    Change handling promise for signup and login from then to await

--- controllers/Auth.mjs
+++ controllers/Auth.mjs
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 import { UserModel } from "../models/UserModel.mjs";
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
@@ -15,65 +15,56 @@ const signup = (req, res, next) => {
     error.data = errors.array();
     throw error;
   }
-
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPassword) => {
-      const user = new UserModel({
-        email: email,
-        password: hashedPassword,
-        name: name,
-      });
-
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({ message: "User created", userId: result._id });
-    })
-    .catch((error) => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new UserModel({
+      email: email,
+      password: hashedPassword,
+      name: name,
     });
+
+    const result = await user.save();
+    res.status(201).json({ message: "User created", userId: result._id });
+  } catch (error) {
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   let loadedUser;
 
-  UserModel.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        const error = new Error("User not found");
-        error.statusCode = 401;
-        throw error;
-      }
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 401;
+      throw error;
+    }
+    loadedUser = user;
 
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((isEqual) => {
-      if (!isEqual) {
-        const error = new Error("Password not match");
-        error.statusCode = 401;
-        throw error;
-      }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Password not match");
+      error.statusCode = 401;
+      throw error;
+    }
 
-      const token = jwt.sign(
-        {
-          email: loadedUser.email,
-          userId: loadedUser._id.toString(),
-        },
-        "YOU THINK IS SECRET",
-        { expiresIn: "1h" }
-      );
-
-      res.status(200).json({ token, userId: loadedUser._id.toString() });
-    })
-    .catch((error) => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    });
+    const token = jwt.sign(
+      {
+        email: loadedUser.email,
+        userId: loadedUser._id.toString(),
+      },
+      "YOU THINK IS SECRET",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ token, userId: loadedUser._id.toString() });
+  } catch (error) {
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 };
 
 export { signup, login };
