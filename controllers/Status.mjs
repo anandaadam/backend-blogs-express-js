c03d5a1fc224ffe0546f7038d00f90385cf2f3b0
adamanandas <adamananda02@gmail.com>
2023-05-06 17:51:19 +0700

    Change handling promise for get and update status from then to await

--- controllers/Status.mjs
+++ controllers/Status.mjs
@@ -1,24 +1,22 @@
 import { UserModel } from "../models/UserModel.mjs";
 import { validationResult } from "express-validator";
 
-const getStatus = (req, res, next) => {
-  UserModel.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        const error = new Error("No user found");
-        error.statusCode = 404;
-        throw error;
-      }
-
-      res.status(200).json({ message: "Success", status: user.status });
-    })
-    .catch((error) => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    });
+const getStatus = async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      const error = new Error("No user found");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ message: "Success", status: user.status });
+  } catch (error) {
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 };
 
-const updateStatus = (req, res, next) => {
+const updateStatus = async (req, res, next) => {
   const status = req.body.status;
   const errors = validationResult(req);
 
@@ -28,24 +26,22 @@ const updateStatus = (req, res, next) => {
     throw error;
   }
 
-  UserModel.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        const error = new Error("No user found");
-        error.statusCode = 404;
-        throw error;
-      }
+  try {
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      const error = new Error("No user found");
+      error.statusCode = 404;
+      throw error;
+    }
 
-      user.status = status;
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({ message: "success" });
-    })
-    .catch((error) => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    });
+    user.status = status;
+    await user.save();
+
+    res.status(201).json({ message: "success" });
+  } catch (error) {
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 };
 
 export { getStatus, updateStatus };
